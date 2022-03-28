bd48997b09a28568e34e28ce2c0ed553580120b0
Sara Cook <saracook087@gmail.com>
2022-03-28 21:00:33 +0100

    refactored code

--- src/booleans.js
+++ src/booleans.js
@@ -1,77 +1,77 @@
 function negate(a) {
   // your code here
   return !a;
-};
+}
 
 function both(a, b) {
   // your code here
-   return (a && b) ? true : false;
-};
+  return a && b;
+}
 
 function either(a, b) {
   // your code here
   return a || b;
-};
+}
 
 function none(a, b) {
   // your code here
-  return (a === false && b === false) ? true : false;
-};
+  return a === false && b === false;
+}
 
 function one(a, b) {
   // your code here
   return a === !b;
-};
+}
 
 function truthiness(a) {
   // your code here
   return Boolean(a);
-};
+}
 
 function isEqual(a, b) {
   // your code here
-  return (a === b) ? true : false;
-};
+  return a === b;
+}
 
 function isGreaterThan(a, b) {
   // your code here
-  return (a > b) ? true : false;
-};
+  return a > b;
+}
 
 function isLessThanOrEqualTo(a, b) {
   // your code here
-  return (a <= b) ? true : false;
-};
+  return a <= b;
+}
 
 function isOdd(a) {
   // your code here
-  return (a % 2 !== 0) ? true : false;
-};
+  return a % 2 !== 0;
+}
 
 function isEven(a) {
   // your code here
-  return (a % 2 === 0) ? true : false; 
-};
+  return a % 2 === 0;
+}
 
 function isSquare(a) {
   // your code here
-  return Number.isInteger(Math.sqrt(a)) ? true : false;
-};
+  return Number.isInteger(Math.sqrt(a));
+}
 
 function startsWith(char, string) {
   // your code here
-  return (char === string.slice(0, 1)) ? true : false;
-};
+  return char === string.slice(0, 1);
+}
 
 function containsVowels(string) {
   // your code here
-  return string.match(/[aeiou]/gi) ? true : false;
-};
+  return !!string.match(/[aeiou]/gi);
+}
 
 function isLowerCase(string) {
   // your code here
-  return string.match(/[A-Z]/g) ? false : true;
-};
+  return !string.match(/[A-Z]/g);
+}
 
 module.exports = {
   negate,
